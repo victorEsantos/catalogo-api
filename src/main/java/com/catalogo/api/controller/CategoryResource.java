1aed5edf3ce275c8fd34175dcfb04128bc83e89e
victor eduardo <victor.eduardo.sntos@gmail.com>
2021-10-13 20:09:36 -0300

    Revert "test"
    This reverts commit 2284bb534c477d22a454fcef691f55547bec5a0c.

--- src/main/java/com/catalogo/api/controller/CategoryResource.java
+++ src/main/java/com/catalogo/api/controller/CategoryResource.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@RequestMapping(value = "/categorie")
+@RequestMapping(value = "/categories")
 @Api(value = "Categoria Controller")
 @CrossOrigin(origins = "*")
 public class CategoryResource implements CategoryControllerOpenAPI {
